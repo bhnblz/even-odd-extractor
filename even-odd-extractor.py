a0adee22f27a220d88a43a844596c1c1f7588fd4
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 20:56:21 +0800

    final output and some python module application

--- even-odd-extractor.py
+++ even-odd-extractor.py
@@ -1,3 +1,11 @@
+import pyfiglet
+
+import colorama
+from colorama import Fore
+
+description = "Program 1: Even-Odd Extractor"
+print(pyfiglet.figlet_format (description.center(5)) + Fore.BLUE)
+
 # Open numbers.txt (read), even.txt (append), odd.txt (append)
 with open("numbers.txt") as main_file, open("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
     # Read numbers.txt line by line 
@@ -9,5 +17,6 @@ with open("numbers.txt") as main_file, open("even.txt", "a") as even_file, open(
         # If odd
         else:
             odd_file.write(str(line))
+            
 # Print the output
 print(numbers)

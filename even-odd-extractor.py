a4694fea7fb0920831e631cbd51efb8e2f125658
Bhea <bheaisipnobleza@gmail.com>
2023-04-27 19:11:33 +0800

    if even

--- even-odd-extractor.py
+++ even-odd-extractor.py
@@ -1,5 +1,10 @@
 # Open numbers.txt (read), even.txt (append), odd.txt (append)
+with open("numbers.txt") as main_file, open("even.txt", "a") as even_file, open("odd.txt", "a") as odd_file:
     # Read numbers.txt line by line 
+    numbers = main_file.readlines()
+    for line in numbers:
         # If even
+        if int(line) % 2 == 0:
+            even_file.write(str(line))
         # If odd
 # Print the output
